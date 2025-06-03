41ac326c2ed42b1c5100ee222af4a1a838f44a5a
roberio5641 <escritorio@avancontabilidade.com.br>
2025-06-03 11:01:07 -0300

    Fast API

--- src/Api_selenium/api.py
+++ src/Api_selenium/api.py
@@ -1,13 +1,16 @@
+# src/Api_selenium/api.py
+
 import platform
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from gclick_scraper import scrape_responsibles
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/', methods=['GET'])
+@app.get("/")
 def scrape():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -15,30 +18,28 @@ def scrape():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument("--single-process")  # Importante para ambientes com poucos recursos
+    chrome_options.add_argument("--single-process")
 
     system = platform.system()
 
-    if system == "windows":
+    if system == "Windows":
         chrome_options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
         chromedriver_path = r"C:\Users\fisca\OneDrive\Documentos\Projeto paineis\selenium-gclick-project\chromedriver-win64\chromedriver.exe"
-    else :
-        chrome_options.binary_location = "/usr/bin/chromium-browser"  # Render usa Chromium
-        chromedriver_path = "/usr/bin/chromedriver"  # Caminho padrão no Render
-        
+    else:
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        chromedriver_path = "/usr/bin/chromedriver"
+    
     service = Service(executable_path=chromedriver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    try: 
+    try:
         dados = scrape_responsibles(driver)
         driver.quit()
 
         if dados:
-            return jsonify({"success": True, "data": dados}), 200
-        else: 
-            return jsonify({"success": False, "message": "Nenhum dado foi coletado."}), 404
+            return {"success": True, "data": dados}
+        else:
+            return JSONResponse(status_code=404, content={"success": False, "message": "Nenhum dado foi coletado."})
     except Exception as e:
         driver.quit()
-        return jsonify({"success": False, "message": str(e)}), 500
-    
-
+        return JSONResponse(status_code=500, content={"success": False, "message": str(e)})
