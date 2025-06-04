a1a51f4101c292f582c9190f90277ebe2356a693
roberio5641 <escritorio@avancontabilidade.com.br>
2025-06-04 07:48:00 -0300

    new build

--- src/Api_selenium/build.sh
+++ src/Api_selenium/build.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 set -e
 
-# Instala dependências do sistema
+# Instala dependências
 apt-get update && apt-get install -y \
-    wget \
-    unzip \
-    libgbm-dev \
-    libxss1 \
-    libasound2 \
-    libnspr4 \
-    libnss3 \
-    fonts-liberation \
-    libatk-bridge2.0-0 \
-    libgtk-3-0 \
-    libx11-xcb1 \
-    ca-certificates \
-    gnupg \
-    curl \
-    xvfb  # Adicionado X virtual framebuffer
+    wget curl gnupg unzip \
+    libgbm-dev libxss1 libasound2 libnspr4 libnss3 \
+    fonts-liberation libatk-bridge2.0-0 libgtk-3-0 libx11-xcb1
 
 # Instala Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 apt-get update && apt-get install -y google-chrome-stable
 
 # Verifica a instalação
-echo "Chrome instalado em: $(which google-chrome)"
-echo "Versão do Chrome: $(google-chrome --version)"
+echo "Chrome instalado em: $(which google-chrome-stable)"
+echo "Versão do Chrome: $(google-chrome-stable --version)"
 
-# Instala Poetry e dependências do Python
-pip install poetry
+# Instala dependências Python
+pip install poetry webdriver-manager
 poetry install
